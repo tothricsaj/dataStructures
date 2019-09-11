f133c65b31bac8db953ef3aab15e1999ce8b260e
tothricsaj <tothricsaj@gmail.com>
2019-09-11 23:27:37 +0200

    Node and LinkedList basic

--- LinkedList/simply/main.py
+++ LinkedList/simply/main.py
@@ -1,2 +1,21 @@
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList():
+    def __init__(self):
+        self.head = None
+
+
 if __name__ == '__main__':
-    print('Hello ll!!!')
+    print('Hello LinkedList!!!\n')
+
+    llist = LinkedList()
+
+    llist.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+
+    llist.head.next = second
+    second.next = third
