cf7035d5857bc4de895a61f868846a36975a9613
tothricsaj <tothricsaj@gmail.com>
2019-09-12 23:43:36 +0200

    js->LinkedList is in module

---
+++ LinkedList/simply/javascript/LinkedList/LinkedList.js
@@ -0,0 +1,30 @@
+function Node(data) {
+	this.data = data;
+	this.next = null;
+}
+
+exports.LinkedList = function() {
+	this.head = null;
+	this.lastNode = null;
+
+	this.push = function(data) {
+		if(this.head === null) {
+			this.head = new Node(data);
+			this.lastNode = this.head;
+
+			return;
+		}
+
+		this.lastNode.next = new Node(data);
+		this.lastNode = this.lastNode.next;
+	}
+
+	this.printAll = function() {
+		tmp = this.head;
+		while(tmp) {
+			console.log(tmp.data);
+			tmp = tmp.next;
+		}
+	}
+}
+
--- LinkedList/simply/javascript/main.js
+++ LinkedList/simply/javascript/main.js
@@ -1,37 +1,9 @@
+var ll = require('./LinkedList/LinkedList.js')
 console.log('Hello LinkedList!!!');
 
-function Node(data) {
-	this.data = data;
-	this.next = null;
-}
-
-function LinkedList() {
-	this.head = null;
-	this.lastNode = null;
-
-	this.push = function(data) {
-		if(this.head === null) {
-			this.head = new Node(data);
-			this.lastNode = this.head;
-
-			return;
-		}
-
-		this.lastNode.next = new Node(data);
-		this.lastNode = this.lastNode.next;
-	}
-
-	this.printAll = function() {
-		tmp = this.head;
-		while(tmp) {
-			console.log(tmp.data);
-			tmp = tmp.next;
-		}
-	}
-}
 
 function main() {
-	let llist = new LinkedList();
+	let llist = new ll.LinkedList();
 
 	llist.push(1);
 	llist.push(2);
