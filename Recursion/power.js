a20c95301c7d421aadff6df59193a5d7d46ae2f8
tothricsaj <tothricsaj@gmail.com>
2020-04-27 20:19:30 +0200

    power

---
+++ Recursion/power.js
@@ -0,0 +1,20 @@
+// power(2,0) // 1
+// power(2,2) // 4
+// power(2,4) // 16
+
+function power(num1, num2){
+    let result = 1
+    
+    if(num2 === 0) return result
+    
+    function helper(counter) {
+        if(counter === 1) return result * num1
+        
+        counter--
+        return num1 * helper(counter)
+    }
+
+    return helper(num2)
+}
+
+console.log(power(2, 4))
