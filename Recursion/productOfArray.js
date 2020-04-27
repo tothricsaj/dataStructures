f327c8d7b40f0bef00a541d660993d961b402bc8
tothricsaj <tothricsaj@gmail.com>
2020-04-27 20:31:09 +0200

    ready

--- Recursion/productOfArray.js
+++ Recursion/productOfArray.js
@@ -1,10 +1,10 @@
 function productOfArray(arr) {
 
     function helper(array) {
-        return array.length === 0 ? 1 : array[0] * helper(arr.slice(1))
+        return array.length === 0 ? 1 : array[0] * helper(array.slice(1))
     }
 
-    helper(arr)
+    return helper(arr)
 }
 
-console.log(productOfArray([1,2,3]))
+console.log(productOfArray([1,2,3,10]))
