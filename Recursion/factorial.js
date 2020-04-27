627ce07d848d189c746a3b7c81ba593abc355533
tothricsaj <tothricsaj@gmail.com>
2020-04-27 20:26:41 +0200

    factorial recursion

---
+++ Recursion/factorial.js
@@ -0,0 +1,11 @@
+function factorial(num){
+    if(num === 0) return 1
+
+    function helper(num) {
+        return num === 1 ? num * 1 : num * helper(num - 1)
+    }
+
+    return helper(num)
+}
+
+console.log(factorial(7))
