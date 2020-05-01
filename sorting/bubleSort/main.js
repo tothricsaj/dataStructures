2c15d803e4b286acda61f13df06f4e1d1c53abfc
tothricsaj <tothricsaj@gmail.com>
2020-05-01 15:37:47 +0200

    console more informaiton

--- sorting/bubleSort/main.js
+++ sorting/bubleSort/main.js
@@ -1,17 +1,16 @@
 function bubbleSort(arr) {
     for(let i = arr.length; i>0; i--) {
         for(let j=0; j<i; j++) {
+            console.log(arr, arr[j], arr[j+1])
             if(arr[j] > arr[j+1]) {
                 let tmp = arr[j]
                 arr[j] = arr[j+1]
                 arr[j+1] = tmp
             }
-
-            console.log(arr)
         }
     }
 
     return arr
 }
 
-console.log(bubbleSort([2,1,4,45,22,12]))
+console.log(bubbleSort([2,1,5,19,10,4,11,12,32,17]))
