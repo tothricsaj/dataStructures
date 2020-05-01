f8334dc0146e20ad975f1815790b0e996d12a462
tothricsaj <tothricsaj@gmail.com>
2020-05-01 16:41:52 +0200

    loops :duck:

---
+++ sorting/selectionSort/main.js
@@ -0,0 +1,10 @@
+function selectionSort(arr) {
+    for(let i=0; i<arr.length; i++) {
+        for(let k=i+1; k<arr.length; k++) {
+
+        }
+    }
+    return arr
+}
+
+console.log(selectionSort([3,2,5,11,8,12]))
