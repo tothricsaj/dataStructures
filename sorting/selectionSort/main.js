e0f6d20a236f9f204ed1ee1cd09cdd697a8e6d2d
tothricsaj <tothricsaj@gmail.com>
2020-05-01 16:45:39 +0200

    selection sorting is ready

--- sorting/selectionSort/main.js
+++ sorting/selectionSort/main.js
@@ -1,8 +1,10 @@
 function selectionSort(arr) {
     for(let i=0; i<arr.length; i++) {
+        let lowest = i
         for(let k=i+1; k<arr.length; k++) {
-
+            if(arr[lowest] > arr[k]) lowest = k
         }
+        if(i !== lowest) [arr[lowest], arr[i]] = [arr[i], arr[lowest]]
     }
     return arr
 }
